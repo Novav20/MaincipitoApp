4c42f9f0987e306df9051eb8902a1285b6e006ee
Novav20 <illus.kun@gmail.com>
2022-07-24 14:43:08 -0500

    Improving DoctorPage

--- Hospi.App.Domain/Entities/Doctor.cs
+++ Hospi.App.Domain/Entities/Doctor.cs
@@ -5,14 +5,14 @@ namespace Hospi.App.Domain.Entities
     [Display(Name ="Médico")]
     public class Doctor : Person
     {
-        [Required, StringLength(50), Display(Name = "Especialidad")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$",ErrorMessage ="aaa")]
+        [Required(ErrorMessage ="Se requiere una especialidad"), StringLength(50), Display(Name = "Especialidad")]
+        [RegularExpression(@"^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$",ErrorMessage ="Solo se admiten letras")]
         public string MedicalSpecialty { get; set; }
-
-        [Required, StringLength(50), Display(Name = "Código")]
+        
+        [Required(ErrorMessage ="Debe proporcionar un código"), StringLength(50), Display(Name = "Código")]
         public string Code { get; set; }
 
-        [Required, StringLength(50), Display(Name = "Registro Rethus")]
+        [Required(ErrorMessage = "Debe proporcionar el registro rethus"), StringLength(50), Display(Name = "Registro Rethus")]
         public string RethusRecord { get; set; }
     }
 
--- Hospi.App.Domain/Entities/Person.cs
+++ Hospi.App.Domain/Entities/Person.cs
@@ -9,16 +9,16 @@ namespace Hospi.App.Domain.Entities
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El campo nombre(s) es requerido")]
-        [RegularExpression(@"^+[a-zA-Z\s]*$", ErrorMessage ="Solo letras son permitidas")]
+        [RegularExpression(@"^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$",ErrorMessage ="Solo se admiten letras")]
         [StringLength(50), Display(Name = "Nombre(s)")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "El campo apellido(s) es requerido")]
-        [RegularExpression(@"^+[a-zA-Z\s]*$", ErrorMessage ="Solo letras son permitidas")]
+        [RegularExpression(@"^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$",ErrorMessage ="Solo se admiten letras")]
         [StringLength(50), Display(Name = "Apellido(s)")]
         public string Surname { get; set; }
 
-        [Required(ErrorMessage = "El campo teléfono es requerido")]
+        [Required(ErrorMessage = "Se requiere un número de télefono")]
         [RegularExpression("([1-9][0-9]*)",ErrorMessage = "Debe introducir el número sin espacios ni caracteres especiales")]
         [StringLength(50), Display(Name = "Teléfono")]
         public string Cellphone { get; set; }
